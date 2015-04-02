120858d7e210dfa64007787520b6215c05ad9e77
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-04-02 11:24:44 -0700

    video 26 done, middle of 27

--- js/screens/title.js
+++ js/screens/title.js
@@ -3,7 +3,19 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10); // TODO
+                
+                me.input.bindKey(me.input.key.ENTER, "start");
+                
+                me.game.world.addChild(new (me.renderable.extend({
+                    init: function() {
+                       this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]) ;
+                    },
+                    
+                    draw: function(renderer){
+                        
+                    }
+                })));
 	},
 	
 	
