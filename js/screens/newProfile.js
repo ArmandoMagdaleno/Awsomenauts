88e5083b4544c209d83d01f38ce1bcd6886bbc1e
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-04-27 12:11:07 -0700

    video 53 done

--- js/screens/newProfile.js
+++ js/screens/newProfile.js
@@ -4,6 +4,8 @@ game.NewProfile = me.ScreenObject.extend({
      */
     onResetEvent: function() {
         me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("new-screen")), -10); // TODO
+        document.getElementById("input").style.visibility = "visible";
+        document.getElementById("register").style.visibility = "visible";
 
         me.input.unbindKey(me.input.KEY.B);
         me.input.unbindKey(me.input.KEY.Q);
@@ -14,7 +16,7 @@ game.NewProfile = me.ScreenObject.extend({
         me.game.world.addChild(new (me.Renderable.extend({
             init: function() {
                 this._super(me.Renderable, 'init', [10, 10, 300, 50]);
-                this.font = new me.Font("Arial", 26, "white");
+                this.font = new me.Font("Arial", 26, "black");
             },
             draw: function(renderer) {
                 this.font.draw(renderer.getContext(), "PICK A USERNAME AND PASSWORD", this.pos.x, this.pos.y);
@@ -28,7 +30,8 @@ game.NewProfile = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
     onDestroyEvent: function() {
-        
+        document.getElementById("input").style.visibility = "hidden";
+        document.getElementById("register").style.visibility = "hidden";
     }
 });
 
